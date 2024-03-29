4c983580998d08aa42ff62efafbe38dd02701323
Denison Resplandes <denison.si2014@gmail.com>
2024-03-29 17:46:35 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,19 @@
+package model.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+	
+	void insert(Seller department);
+	
+	void update(Seller department);
+	
+	void deleteById(Integer id);
+	
+	Optional<Seller> findById(Integer id);
+	
+	List<Seller> findAll();	
+}
